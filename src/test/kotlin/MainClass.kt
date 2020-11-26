ad312ca7f7df8f3bbdc68e2a5979d8f964bf8b23
Dmitry Kozlov <thaidem19@gmail.com>
2020-11-26 15:13:08 +0300

    Исправления 1/1 1/2

--- src/test/kotlin/MainClass.kt
+++ src/test/kotlin/MainClass.kt
@@ -1,7 +1,12 @@
 open class MainClass {
 
+    private val class_number = 20
+
+    fun getClassNumber(): Int {
+        return class_number
+    }
+
     fun getLocalNumber(): Int {
         return 14
-
     }
 }
--- src/test/kotlin/MainClassTest.kt
+++ src/test/kotlin/MainClassTest.kt
@@ -6,10 +6,18 @@ class MainClassTest : MainClass() {
     @Test
     fun testGetLocalNumber() {
 
-        val checkLocalNumber = 14
         val localNumber = super.getLocalNumber()
+        val checkLocalNumber = 14
 
-        assertTrue("$localNumber != $checkLocalNumber", super.getLocalNumber() == checkLocalNumber)
+        assertTrue("$localNumber != $checkLocalNumber", localNumber == checkLocalNumber)
+    }
 
+    @Test
+    fun testGetClassNumber() {
+
+        val classNumber = super.getClassNumber()
+        val checkClassNumber = 45
+
+        assertTrue("$classNumber not more $checkClassNumber", classNumber > checkClassNumber)
     }
 }
